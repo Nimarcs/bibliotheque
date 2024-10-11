123f155a5c35ea1ee978a8cc3a22d0135b2050c1
lopesvaz3u <82098184+lopesvaz3u@users.noreply.github.com>
2024-10-11 11:51:55 +0200

    test hello world

---
+++ src/main/java/fr/miage/am/bibliotheque/HelloWorldController.java
@@ -0,0 +1,13 @@
+package fr.miage.am.bibliotheque;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "<b>Hello, World!</b>";
+    }
+}
