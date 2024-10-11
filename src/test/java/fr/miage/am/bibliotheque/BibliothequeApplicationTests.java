1475cec61775f3b6301cc1c5be64934044055be4
Nimarcs <moimonautremoi@gmail.com>
2024-10-11 11:39:26 +0200

    Init

---
+++ src/test/java/fr/miage/am/bibliotheque/BibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.am.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
