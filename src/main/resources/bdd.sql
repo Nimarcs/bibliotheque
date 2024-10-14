b68664c9efe81396c90f9c4640778feea510c46c
lopesvaz3u <82098184+lopesvaz3u@users.noreply.github.com>
2024-10-14 18:11:39 +0200

    ajout fichier de création de bdd

---
+++ src/main/resources/bdd.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS bibliotheque;
+USE bibliotheque;
+
+CREATE TABLE IF NOT EXISTS usager(
+                                     id INT NOT NULL AUTO_INCREMENT,
+                                     identifiant VARCHAR(50) NOT NULL,
+                                     nom VARCHAR(50) NOT NULL,
+                                     prenom VARCHAR(50) NOT NULL,
+                                     numTel VARCHAR(50) NOT NULL,
+                                     mail VARCHAR(50) NOT NULL,
+                                     adresse VARCHAR(50) NOT NULL,
+                                     PRIMARY KEY (id)
+);
+
+INSERT INTO usager (identifiant, nom, prenom, numTel, mail, adresse)
+VALUES ('u12345', 'Doe', 'John', '0123456789', 'john.doe@example.com', '123 Rue de l\'Exemple');
+
+SELECT * FROM usager;
